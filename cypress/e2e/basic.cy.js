275a409b4115716f8d4e32df93df12598b426ef5
Alex Alexandre Alves <alex.alexandre@ngi.com.br>
2023-01-23 12:01:48 -0300

    feat: invoke, and resolve todos

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,20 +1,34 @@
 /// <reference types="cypress" />
 
 describe('Cypress basics', () => {
-    it('Should visit a page and assert title', () => {
+    it.only('Should visit a page and assert title', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
 
         cy.title()
         .should('be.equal', 'Campo de Treinamento')
         .and('contain', 'Campo')
         
-        cy.title().should(title => {
+        let syncTitle
+
+        cy.title().then(title => {
             console.log(title)
+
+            cy.get('#formNome').type(title)
+
+            syncTitle = title
+        })
+
+        cy.get('[data-cy="dataSobrenome"]').then($el => {
+            $el.val(syncTitle)
+        })
+
+        cy.get('#elementosForm\\:sugestoes').then($el => {
+            cy.wrap($el).type(syncTitle)
         })
 
     })
 
-    it.only('Should find and interact with an element ', () => {
+    it('Should find and interact with an element ', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
         cy.get('#buttonSimple').debug()
         .click() 
