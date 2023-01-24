e1da356113dcd26ec3cf6b16a8dbaea640c5b1c2
Alex Alexandre Alves <alex.alexandre@ngi.com.br>
2023-01-24 11:42:00 -0300

    feat: alert, iframe, popup

--- cypress/e2e/alert.cy.js
+++ cypress/e2e/alert.cy.js
@@ -56,7 +56,7 @@ describe('Work with basic alerts', () => {
         cy.get('#confirm').click()
     })
 
-    it.only('Prompt', () => {
+    it('Prompt', () => {
         cy.window().then(win => {
             cy.stub(win, 'prompt').returns('42').as('Window')
         })
@@ -73,5 +73,36 @@ describe('Work with basic alerts', () => {
         cy.get('#prompt').click()
     })
 
+    it('Validando mensagens', () => {
+
+        const stub = cy.stub().as('alert')
+        cy.on('window:alert', stub)
+        cy.get('#formCadastrar').click()
+            .then(() => {
+                expect(stub.getCall(0)).to.be.calledWith('Nome eh obrigatorio')
+            })
+
+        cy.get('#formNome').type('Alex')
+        cy.get('#formCadastrar').click()
+        .then(() => {
+            expect(stub.getCall(1)).to.be.calledWith('Sobrenome eh obrigatorio')
+        })
+
+        cy.get('[data-cy="dataSobrenome"]').type('Alexandre')
+        cy.get('#formCadastrar').click()
+        .then(() => {
+            expect(stub.getCall(2)).to.be.calledWith('Sexo eh obrigatorio')
+        })
+
+        cy.get('#formSexoFem').click()
+        cy.get('#formCadastrar').click()
+
+        cy.get('#resultado > :nth-child(1)').should('have.text', 'Cadastrado!')
+    })
+
 })
+
+
+
+
 
